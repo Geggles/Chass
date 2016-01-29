99fda6ac6b10993558289fd75931f953ff55a9d2
Geggles <geggles@live.de>
2016-01-29 23:00:22 +0100

    Implement color getter and setter

--- Game/Square.java
+++ Game/Square.java
@@ -1,7 +1,16 @@
 package Game;
 
 public class Square {
+    private Colors color;
     public Square(){
 
+    }
+
+    public void setColor(Colors color){
+        this.color = color;
+    }
+
+    public Colors setColor(){
+        return color;
     }
 }
