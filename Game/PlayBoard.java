8e0f8e64e230a438efab955a3e9994ce182f51dc
Geggles <geggles@live.de>
2016-01-29 22:35:38 +0100

    Establish Subclassing

--- Game/PlayBoard.java
+++ Game/PlayBoard.java
@@ -3,7 +3,7 @@ package Game;
 /**
 * Used for Boards Alpha, Beta and Gamma
 * */
-public abstract class PlayBoard {
+public abstract class PlayBoard extends Board{
     abstract void move(Square source, Square destination);
     abstract boolean validMove(Square source, Square destination);
     abstract void capture(Square source, Square destination);
