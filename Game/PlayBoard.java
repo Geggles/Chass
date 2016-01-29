6f6e12cfbdada6aac6004876d7a939826b554666
Geggles <geggles@live.de>
2016-01-29 22:58:35 +0100

    Setup Squares with colors

--- Game/PlayBoard.java
+++ Game/PlayBoard.java
@@ -7,4 +7,19 @@ public abstract class PlayBoard extends Board{
     abstract void move(Square source, Square destination);
     abstract boolean validMove(Square source, Square destination);
     abstract void capture(Square source, Square destination);
+
+    public PlayBoard(){
+        squares = new Square[8][8];
+        Colors currentColor = Colors.WHITE;
+        for (int row=0; row<8; row++){
+            for (int column=0; column<8; column++) {
+                squares[row][column].setColor(currentColor);
+                if (currentColor == Colors.WHITE) {
+                    currentColor = Colors.BLACK;
+                } else {
+                    currentColor = Colors.WHITE;
+                }
+            }
+        }
+    }
 }
