80a9feae238924af6c737a1ae4437fc6d54bd9a8
Geggles <geggles@live.de>
2016-01-29 22:06:58 +0100

    Add necessary methods

--- Game/SpecialBoard.java
+++ Game/SpecialBoard.java
@@ -7,4 +7,16 @@ public class SpecialBoard extends PlayBoard{
     public SpecialBoard() {
 
     }
+
+    public void move(Square source, Square destination){
+
+    }
+
+    public boolean validMove(Square source, Square destination){
+        return true;
+    }
+
+    public void capture(Square source, Square destination){
+
+    }
 }
--- Game/StandardBoard.java
+++ Game/StandardBoard.java
@@ -3,5 +3,21 @@ package Game;
 /**
  * Used for boards Alpha and Beta
  * */
-public abstract class StandardBoard extends PlayBoard{
+public class StandardBoard extends PlayBoard{
+    public StandardBoard(){
+
+    }
+
+    public void move(Square source, Square destination){
+
+    }
+
+    public boolean validMove(Square source, Square destination){
+        return true;
+    }
+
+    public void capture(Square source, Square destination){
+
+    }
+
 }
