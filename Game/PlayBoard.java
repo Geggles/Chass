67f174e5ae55bbc398f10240fb92c458565e1172
Geggles <geggles@live.de>
2016-01-29 21:29:08 +0100

    Add some mandatory methods for play boards

--- Game/PlayBoard.java
+++ Game/PlayBoard.java
@@ -4,4 +4,7 @@ package Game;
 * Used for Boards Alpha, Beta and Gamma
 * */
 public abstract class PlayBoard {
+    abstract void move(Square source, Square destination);
+    abstract boolean validMove(Square source, Square destination);
+    abstract void capture(Square source, Square destination);
 }
