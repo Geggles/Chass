9b471d5e99300a46af1a4d4c714dfbcaba99255c
Geggles <geggles@live.de>
2016-01-29 19:07:00 +0100

    Initial Structure

--- GUI/Board.java
+++ GUI/Board.java
@@ -1,6 +1,10 @@
 package GUI;
 public class Board {
-    public Board(){
+    /**
+    * Signals signals: reference to the Signals object that contains signals that are can to be emitted by this class
+    * Game.Board gameBoard: reference to the Board object that contains the game mechanics
+    * */
+    public Board(Signals signals, Game.Board gameBoard){
 
     }
 }
