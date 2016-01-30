564c26f6359e1802d8daa8cb9b1ff810a9fb1ac3
Geggles <geggles@live.de>
2016-01-30 14:04:29 +0100

    Values of Pieces

---
+++ Game/PieceValue.java
@@ -0,0 +1,20 @@
+package Game;
+
+public enum PieceValue {
+    PAWN(1),
+    BISHOP(3),
+    KNIGHT(3),
+    ROOK(5),
+    QUEEN(9),
+    KING(-1);
+
+    private int value;
+
+    PieceValue(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
