fec45d015327f5e6fc5627d172299bffe4936c24
Geggles <geggles@live.de>
2016-01-30 17:21:07 +0100

    Add getPiece method

--- Game/Board.java
+++ Game/Board.java
@@ -1,13 +1,32 @@
 package Game;
 
+import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+
+import java.util.ArrayList;
 
 public abstract class Board {
     private HashBiMap<Square, Piece> state = HashBiMap.create(32);
     private HashBiMap<Square, int[]> squares;
-    public abstract int[] getCoordinate(Square square);
+    protected abstract void setupSquares();
 
-    public void setSquareAt(int[] coordinates, Square square){
+    protected void initializeSquares(int size){
+        squares = HashBiMap.create(size);
+    }
+
+    public Piece getPieceOn(Square square){
+        return state.get(square);
+    }
+
+    public Piece getPieceOn(int[] coordinates){
+        return getPieceOn(getSquareAt(coordinates));
+    }
+
+    public Square getSquareOf(Piece piece){
+        return state.inverse().get(piece);
+    }
+
+    protected void addSquareAt(int[] coordinates, Square square){
         squares.inverse().put(coordinates, square);
     }
 
@@ -15,11 +34,33 @@ public abstract class Board {
         return squares.inverse().get(coordinates);
     }
 
-    public void setCoordinateOf(int[] coordinates, Square square){
+    protected void addCoordinateOf(int[] coordinates, Square square){
         squares.put(square, coordinates);
     }
 
     public int[] getCoordinateOf(Square square){
         return squares.get(square);
+    }
+
+    public void setPieceAt(Piece piece, Square square){
+        state.put(square, piece);
+    }
+
+    public void setPieceAt(Piece piece, int[] coordinates){
+        setPieceAt(piece, getSquareAt(coordinates));
+    }
+
+    public void removePieceFrom(Square square){
+        state.put(square, null);
+    }
+
+    public void removePieceFrom(int[] coordinates){
+        removePieceFrom(getSquareAt(coordinates));
+    }
+
+    public ArrayList<Piece> getAllPieces() {
+        ArrayList<Piece> res = new ArrayList<>();
+        state.forEach((square, piece) -> res.add(piece));
+        return res;
     }
 }
