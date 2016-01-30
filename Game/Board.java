8b11a2b961231a2309a8883b859984dcb2243429
Geggles <geggles@live.de>
2016-01-30 13:05:24 +0100

    Add square hashbimap attribute and accessors

--- Game/Board.java
+++ Game/Board.java
@@ -3,6 +3,23 @@ package Game;
 import com.google.common.collect.HashBiMap;
 
 public abstract class Board {
-    HashBiMap<Square, Piece> state = HashBiMap.create(32);
-    Square[][] squares;
+    private HashBiMap<Square, Piece> state = HashBiMap.create(32);
+    private HashBiMap<Square, int[]> squares;
+    public abstract int[] getCoordinate(Square square);
+
+    public void setSquareAt(int[] coordinates, Square square){
+        squares.inverse().put(coordinates, square);
+    }
+
+    public Square getSquareAt(int[] coordinates){
+        return squares.inverse().get(coordinates);
+    }
+
+    public void setCoordinateOf(int[] coordinates, Square square){
+        squares.put(square, coordinates);
+    }
+
+    public int[] getCoordinateOf(Square square){
+        return squares.get(square);
+    }
 }
