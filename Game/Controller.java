7ae6e7522064a7990c8b1f6b310519bcfa18f317
Geggles <geggles@live.de>
2016-01-31 00:16:16 +0100

    Implement moving pieces

--- Game/Controller.java
+++ Game/Controller.java
@@ -9,13 +9,13 @@ public class Controller {
     StandardBoard alpha;
     StandardBoard beta;
     SpecialBoard gamma;
-    HashMap<Colors, Prison> prisons;
-    HashMap<Colors, Airfield> airfields;
-    Colors turnPlayer = Colors.WHITE;
+    HashMap<Color, Prison> prisons;
+    HashMap<Color, Airfield> airfields;
+    Color turnPlayer = Color.WHITE;
 
     public Controller() {
-        alpha = new StandardBoard(Colors.WHITE);
-        beta = new StandardBoard(Colors.BLACK);
+        alpha = new StandardBoard(Color.WHITE);
+        beta = new StandardBoard(Color.BLACK);
         gamma = new SpecialBoard();
         prisons = new HashMap<>(2);
         airfields = new HashMap<>(2);
@@ -30,7 +30,64 @@ public class Controller {
     }
 
     public void move(Square source, Square destination){
+        move(source, destination, source.board);
+    }
 
+    public void move(Square source, Square destination, Board destinationBoard){
+        PlayBoard sourceBoard = (PlayBoard) source.board;
+        Piece piece = sourceBoard.getPieceOn(source);
+
+        sourceBoard.removePieceFrom(source);
+        if(destinationBoard.getPieceOn(sourceBoard.getCoordinateOf(destination)) == null){
+            destinationBoard.setPieceAt(piece, sourceBoard.getCoordinateOf(destination));
+        }else {
+            sourceBoard.setPieceAt(piece, destination);
+        }
+    }
+
+    /**
+     * Do special moves en passant or casteling as applicable.
+     * Return true if special move has been made; false otherwise.
+     * */
+    public boolean doSpecialMove(Square source, Square destination){
+        StandardBoard board = (StandardBoard) source.board;
+        return false;
+    }
+
+    public void castle(Color color, Value side){
+        Piece king;
+        Piece rook;
+        if (color == Color.WHITE){
+            king = alpha.getPieceOn(new int[] {0, 4});
+            alpha.removePiece(king);
+            if (side == Value.QUEEN){
+                rook = alpha.getPieceOn(new int[] {0, 0});
+                alpha.removePiece(rook);
+                alpha.setPieceAt(king, new int[] {0, 2});
+                alpha.setPieceAt(rook, new int[] {0, 4});
+            }else {
+            rook = alpha.getPieceOn(new int[] {0, 7});
+            alpha.removePiece(rook);
+            alpha.setPieceAt(king, new int[] {0, 6});
+            alpha.setPieceAt(rook, new int[] {0, 5});
+            }
+
+        }else {
+            king = beta.getPieceOn(new int[]{7, 4});
+            beta.removePiece(king);
+            if (side == Value.QUEEN) {
+                rook = beta.getPieceOn(new int[]{7, 0});
+                beta.removePiece(rook);
+                beta.setPieceAt(king, new int[]{7, 2});
+                beta.setPieceAt(rook, new int[]{7, 4});
+            }
+            rook = beta.getPieceOn(new int[]{7, 7});
+            beta.removePiece(rook);
+            beta.setPieceAt(king, new int[]{7, 6});
+            beta.setPieceAt(rook, new int[]{7, 5});
+        }
+        king.canCastle = false;
+        rook.canCastle = false;
     }
 
     /**
@@ -46,8 +103,8 @@ public class Controller {
         try { // for alpha and beta
             StandardBoard standardBoard = (StandardBoard)board;
             if (standardBoard.isPinned(source)) return false;
-            if (piece.value == PieceValue.KING &&
-                (standardBoard.isUnderAttack(destination, Colors.switchColors(piece.color)))) {
+            if (piece.value == Value.KING &&
+                (standardBoard.isUnderAttack(destination, Color.switchColors(piece.color)))) {
                     return false;
             }
             return true;
@@ -65,7 +122,8 @@ public class Controller {
                              PlayBoard destinationBoard) {
 
         if (!validMove(source, destination)) return false;
-        if (destinationBoard.getPieceOn(source.board.getCoordinateOf(destination))!=null){
+        ExtraBoard sourceBoard = (ExtraBoard)source.board;
+        if (destinationBoard.getPieceOn(sourceBoard.getCoordinateOf(destination))!=null){
             return false;
         }
         return true;
