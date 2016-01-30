e88b58c6b2230d1b10b0802a55c94b4449104c12
Geggles <geggles@live.de>
2016-01-30 12:29:14 +0100

    Add color attribute

--- Game/Piece.java
+++ Game/Piece.java
@@ -1,4 +1,17 @@
 package Game;
 
 public class Piece {
+    private Colors color;
+
+    public Piece(){
+
+    }
+
+    public void setColor(Colors color){
+        this.color = color;
+    }
+
+    public Colors getColor(){
+        return color;
+    }
 }
