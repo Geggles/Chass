99fb5fb57585c591318260f9f9586796fa807cc4
Geggles <geggles@live.de>
2016-01-30 15:49:10 +0100

    Add method to setup squares

--- Game/PlayBoard.java
+++ Game/PlayBoard.java
@@ -4,16 +4,22 @@ package Game;
 * Used for Boards Alpha, Beta and Gamma
 * */
 public abstract class PlayBoard extends Board{
-    abstract void move(Square source, Square destination);
-    abstract boolean validMove(Square source, Square destination);
-    abstract void capture(Square source, Square destination);
-
     public PlayBoard(){
-        squares = new Square[8][8];
-        Colors currentColor = Colors.WHITE;
-        for (int row=0; row<8; row++){
-            for (int column=0; column<8; column++) {
-                squares[row][column].setColor(currentColor);
+        initializeSquares(64);
+        setupSquares();
+    }
+
+    protected void setupSquares(){
+        Square square;
+        int[] coordinate;
+        Colors currentColor = Colors.BLACK;
+        for (int row=0; row<8; row++) {
+            for (int column = 0; column < 8; column++) {
+                square = new Square();
+                coordinate = new int[] {row, column};
+                square.setColor(currentColor);
+                addSquareAt(coordinate, square);
+
                 if (currentColor == Colors.WHITE) {
                     currentColor = Colors.BLACK;
                 } else {
