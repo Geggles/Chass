14e09e74bbda595c9402f8826244cab115acd5a1
Geggles <geggles@live.de>
2016-01-30 11:18:52 +0100

    Add Board attribute

--- Game/Square.java
+++ Game/Square.java
@@ -2,6 +2,8 @@ package Game;
 
 public class Square {
     private Colors color;
+    private Board board;
+
     public Square(){
 
     }
@@ -9,8 +11,14 @@ public class Square {
     public void setColor(Colors color){
         this.color = color;
     }
-
-    public Colors setColor(){
+    public Colors getColor(){
         return color;
+    }
+
+    public void setBoard(Board board){
+        this.board = board;
+    }
+    public Board getBoard(){
+        return board;
     }
 }
