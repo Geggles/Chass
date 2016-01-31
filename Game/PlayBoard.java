d931f7ad817055382f2f6a27ab08d027859263e9
Geggles <geggles@live.de>
2016-01-31 09:32:19 +0100

    Add square names

--- Game/PlayBoard.java
+++ Game/PlayBoard.java
@@ -4,6 +4,16 @@ package Game;
 * Used for Boards Alpha, Beta and Gamma
 * */
 public abstract class PlayBoard extends Board{
+    private final String[][] squareNames ={
+            {"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",},
+            {"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",},
+            {"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",},
+            {"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",},
+            {"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",},
+            {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",},
+            {"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",},
+            {"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8",},
+    };
     public PlayBoard(){
         initializeSquares(64);
         setupSquares();
@@ -160,5 +170,14 @@ public abstract class PlayBoard extends Board{
         if (piece.color != color) return 0;
         if (piece.value != value) return 0;
         return 1;
+    }
+
+    /**
+     * (0,0) -> "a1"
+     * */
+    public String getNameForCoordinates(int[] coordinates){
+        int row = coordinates[0];
+        int column = coordinates[1];
+        return squareNames[row][column];
     }
 }
