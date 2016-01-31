56ec026a5641156ddb364464e43cb7a72b5c6d2e
Geggles <geggles@live.de>
2016-01-31 23:32:03 +0100

    Add a method

--- Game/Prison.java
+++ Game/Prison.java
@@ -1,10 +1,21 @@
 package Game;
 
 public class Prison extends ExtraBoard{
-    public Prison(){
-
+    public Prison(String name, Color color){
+        super(name, color);
     }
     protected void setupSquares(){
 
+    }
+
+    public Square getEmptyCell(){
+        for (int row=0; row<8; row++){
+            for (int column=0; column<8; column++){
+                if (getPieceOn(new int[] {row, column}) == null) {
+                    return getSquareAt(new int[] {row, column});
+                }
+            }
+        }
+        return null;
     }
 }
