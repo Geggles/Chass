be023cc20a9cbb6dab5f02719390d43efc9fc2a3
Geggles <geggles@live.de>
2016-01-31 23:34:45 +0100

    Inverse name access

--- Game/Value.java
+++ Game/Value.java
@@ -1,17 +1,31 @@
 package Game;
 
 public enum Value {
-    PAWN(1),
-    BISHOP(3),
-    KNIGHT(3),
-    ROOK(5),
-    QUEEN(9),
-    KING(-1);
+    PAWN(1, 'P'),
+    BISHOP(3, 'B'),
+    KNIGHT(3, 'N'),
+    ROOK(5, 'R'),
+    QUEEN(9, 'Q'),
+    KING(-1, 'K');
 
     public final int value;
+    public final Character name;
 
-    Value(int value){
+    Value(int value, Character name){
         this.value = value;
+        this.name = name;
+    }
+
+    public static Value getValueFromName(Character name){
+        switch (name){
+            case 'P': return PAWN;
+            case 'B': return BISHOP;
+            case 'N': return KNIGHT;
+            case 'R': return ROOK;
+            case 'Q': return QUEEN;
+            case 'K': return KING;
+            default: return null;
+        }
     }
 
 /*    public int getValue(){
