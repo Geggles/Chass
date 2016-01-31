aa6b0986adb9166b58cf82f872f94f9c8bbf0aaa
Geggles <geggles@live.de>
2016-01-31 23:27:28 +0100

    Pinning is now checked every time

--- Game/Piece.java
+++ Game/Piece.java
@@ -4,7 +4,6 @@ public class Piece {
     public Color color;
     public boolean canNullMove = true;
     public boolean canCastle = true;
-    public int pinned = 0;
     public final Value value;
 
     public Piece(Value value){
