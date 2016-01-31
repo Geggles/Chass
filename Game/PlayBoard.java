19a000243c6ea90639ccbefa00aa4d0fd1f62a9c
Geggles <geggles@live.de>
2016-01-31 12:34:13 +0100

    Refine Square names

--- Game/PlayBoard.java
+++ Game/PlayBoard.java
@@ -1,20 +1,84 @@
 package Game;
 
+import com.google.common.collect.HashBiMap;
+
+import java.util.HashMap;
+
 /**
 * Used for Boards Alpha, Beta and Gamma
 * */
 public abstract class PlayBoard extends Board{
-    private final String[][] squareNames ={
-            {"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",},
-            {"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",},
-            {"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",},
-            {"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",},
-            {"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",},
-            {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",},
-            {"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",},
-            {"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8",},
-    };
-    public PlayBoard(){
+    private static final HashBiMap<int[], String> squareNames = HashBiMap.create(64);
+    static
+    {
+        squareNames.put(new int[] {0, 0}, "a1");
+        squareNames.put(new int[] {0, 1}, "b1");
+        squareNames.put(new int[] {0, 2}, "c1");
+        squareNames.put(new int[] {0, 3}, "d1");
+        squareNames.put(new int[] {0, 4}, "e1");
+        squareNames.put(new int[] {0, 5}, "f1");
+        squareNames.put(new int[] {0, 6}, "g1");
+        squareNames.put(new int[] {0, 7}, "h1");
+        squareNames.put(new int[] {1, 0}, "a2");
+        squareNames.put(new int[] {1, 1}, "b2");
+        squareNames.put(new int[] {1, 2}, "c2");
+        squareNames.put(new int[] {1, 3}, "d2");
+        squareNames.put(new int[] {1, 4}, "e2");
+        squareNames.put(new int[] {1, 5}, "f2");
+        squareNames.put(new int[] {1, 6}, "g2");
+        squareNames.put(new int[] {1, 7}, "h2");
+        squareNames.put(new int[] {2, 0}, "a3");
+        squareNames.put(new int[] {2, 1}, "b3");
+        squareNames.put(new int[] {2, 2}, "c3");
+        squareNames.put(new int[] {2, 3}, "d3");
+        squareNames.put(new int[] {2, 4}, "e3");
+        squareNames.put(new int[] {2, 5}, "f3");
+        squareNames.put(new int[] {2, 6}, "g3");
+        squareNames.put(new int[] {2, 7}, "h3");
+        squareNames.put(new int[] {3, 0}, "a4");
+        squareNames.put(new int[] {3, 1}, "b4");
+        squareNames.put(new int[] {3, 2}, "c4");
+        squareNames.put(new int[] {3, 3}, "d4");
+        squareNames.put(new int[] {3, 4}, "e4");
+        squareNames.put(new int[] {3, 5}, "f4");
+        squareNames.put(new int[] {3, 6}, "g4");
+        squareNames.put(new int[] {3, 7}, "h4");
+        squareNames.put(new int[] {4, 0}, "a5");
+        squareNames.put(new int[] {4, 1}, "b5");
+        squareNames.put(new int[] {4, 2}, "c5");
+        squareNames.put(new int[] {4, 3}, "d5");
+        squareNames.put(new int[] {4, 4}, "e5");
+        squareNames.put(new int[] {4, 5}, "f5");
+        squareNames.put(new int[] {4, 6}, "g5");
+        squareNames.put(new int[] {4, 7}, "h5");
+        squareNames.put(new int[] {5, 0}, "a6");
+        squareNames.put(new int[] {5, 1}, "b6");
+        squareNames.put(new int[] {5, 2}, "c6");
+        squareNames.put(new int[] {5, 3}, "d6");
+        squareNames.put(new int[] {5, 4}, "e6");
+        squareNames.put(new int[] {5, 5}, "f6");
+        squareNames.put(new int[] {5, 6}, "g6");
+        squareNames.put(new int[] {5, 7}, "h6");
+        squareNames.put(new int[] {6, 0}, "a7");
+        squareNames.put(new int[] {6, 1}, "b7");
+        squareNames.put(new int[] {6, 2}, "c7");
+        squareNames.put(new int[] {6, 3}, "d7");
+        squareNames.put(new int[] {6, 4}, "e7");
+        squareNames.put(new int[] {6, 5}, "f7");
+        squareNames.put(new int[] {6, 6}, "g7");
+        squareNames.put(new int[] {6, 7}, "h7");
+        squareNames.put(new int[] {7, 0}, "a8");
+        squareNames.put(new int[] {7, 1}, "b8");
+        squareNames.put(new int[] {7, 2}, "c8");
+        squareNames.put(new int[] {7, 3}, "d8");
+        squareNames.put(new int[] {7, 4}, "e8");
+        squareNames.put(new int[] {7, 5}, "f8");
+        squareNames.put(new int[] {7, 6}, "g8");
+        squareNames.put(new int[] {7, 7}, "h8");
+    }
+
+    public PlayBoard(String name, Color color){
+        super(name, color);
         initializeSquares(64);
         setupSquares();
     }
@@ -25,16 +89,10 @@ public abstract class PlayBoard extends Board{
         Color currentColor = Color.BLACK;
         for (int row=0; row<8; row++) {
             for (int column = 0; column < 8; column++) {
-                square = new Square();
+                square = new Square(currentColor, this);
                 coordinate = new int[] {row, column};
-                square.color = currentColor;
                 addSquareAt(coordinate, square);
-
-                if (currentColor == Color.WHITE) {
-                    currentColor = Color.BLACK;
-                } else {
-                    currentColor = Color.WHITE;
-                }
+                currentColor = Color.switchColors(currentColor);
             }
         }
     }
@@ -175,9 +233,19 @@ public abstract class PlayBoard extends Board{
     /**
      * (0,0) -> "a1"
      * */
-    public String getNameForCoordinates(int[] coordinates){
-        int row = coordinates[0];
-        int column = coordinates[1];
-        return squareNames[row][column];
+    public String getSquareName(int[] coordinates){
+        return squareNames.get(coordinates);
+    }
+
+    public String getSquareName(Square square){
+        return getSquareName(getCoordinateOf(square));
+    }
+
+    public Square getSquareAt(String squareName){
+        return getSquareAt(getCoordinatesOf(squareName));
+    }
+
+    public int[] getCoordinatesOf(String squareName){
+        return squareNames.inverse().get(squareName);
     }
 }
