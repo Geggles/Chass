46f6f27ced621a09f568318d6d0d08b7a0b12fc2
Geggles <geggles@live.de>
2016-01-31 23:25:17 +0100

    Add constructor with name color and a function to get the first piece of certain value, since pieces lose their identity on Extra Boards

--- Game/ExtraBoard.java
+++ Game/ExtraBoard.java
@@ -4,6 +4,15 @@ package Game;
  * Used for Prisons and Airfields
  */
 public abstract class ExtraBoard extends Board{
-    public ExtraBoard(){
+    public ExtraBoard(String name, Color color){
+        super(name, color);
+    }
+
+    public Piece getPiece(Value value){
+        for (Piece piece :
+                getAllPieces()) {
+            if (piece.value == value) return piece;
+        }
+        return null;
     }
 }
