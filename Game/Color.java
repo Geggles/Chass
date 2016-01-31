b2b85f7f042739af208931655e519c083c1023be
Geggles <geggles@live.de>
2016-01-31 23:23:46 +0100

    Rename

--- Game/Color.java
+++ Game/Color.java
@@ -3,7 +3,7 @@ package Game;
 public enum Color {
     BLACK,
     WHITE;
-    static Color switchColors(Color color){
+    static Color oppositeColor(Color color){
         if (color == WHITE) return BLACK;
         return WHITE;
     }
