29a6f64d28999bbdcd346ccab08c5e592b03e7d0
Geggles <geggles@live.de>
2016-02-11 19:08:32 +0100

    Renames

--- Game/SpecialBoard.java
+++ Game/SpecialBoard.java
@@ -7,9 +7,9 @@ public class SpecialBoard extends PlayBoard{
     public SpecialBoard() {
         super("C", null);
     }
-    public Square getSquareAt(int[] coordinates){
+    public Square getSquare(int[] coordinates){
         int height = coordinates[0] % 8;
         int width = coordinates[1] % 8;
-        return super.getSquareAt(new int[] {height, width});
+        return super.getSquare(new int[] {height, width});
     }
 }
