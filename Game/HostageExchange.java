ea7ec3e2e5850c43932386bd7816065ddfdfba80
Geggles <geggles@live.de>
2016-02-11 17:13:26 +0100

    Add defining information

--- Game/HostageExchange.java
+++ Game/HostageExchange.java
@@ -1,7 +1,11 @@
 package Game;
 
 public class HostageExchange extends Move{
-    public HostageExchange(Color player, Prison board, Value Hostage, Square droppedTo){
+    public final Value hostage;
+    public final Square square;
+    public HostageExchange(Color player, Prison board, Value hostage, Square square){
         super(player, board);
+        this.hostage = hostage;
+        this.square = square;
     }
 }
