7b531fa16a025f7abbdf5020bc80d5feacede803
Geggles <geggles@live.de>
2016-02-11 17:01:50 +0100

    Initial

---
+++ Game/HostageExchange.java
@@ -0,0 +1,7 @@
+package Game;
+
+public class HostageExchange extends Move{
+    public HostageExchange(Color player, Prison board, Value Hostage, Square droppedTo){
+        super(player, board);
+    }
+}
