686c51daae14acb163f37da768addf626c821829
Geggles <geggles@live.de>
2016-02-11 17:00:01 +0100

    Tidy up

--- Game/Board.java
+++ Game/Board.java
@@ -21,57 +21,126 @@ public abstract class Board {
         squares = HashBiMap.create(size);
     }
 
-    public Piece getPieceOn(Square square){
-        return state.get(square);
-    }
-
-    public Piece getPieceOn(int[] coordinates){
-        return getPieceOn(getSquareAt(coordinates));
-    }
-
-    public Square getSquareOf(Piece piece){
-        return state.inverse().get(piece);
-    }
-
     protected void addSquareAt(int[] coordinates, Square square){
         squares.inverse().put(coordinates, square);
-    }
-
-    public Square getSquareAt(int[] coordinates){
-        return squares.inverse().get(coordinates);
     }
 
     protected void addCoordinatesOf(int[] coordinates, Square square){
         squares.put(square, coordinates);
     }
 
-    public int[] getCoordinatesOf(Square square){
+
+
+    public Square getSquare(Piece piece){
+        return state.inverse().get(piece);
+    }
+
+    public Square getSquare(int[] coordinates){
+        return squares.inverse().get(coordinates);
+    }
+
+    public Square getSquare(int row, int column){
+        return getSquare(new int[] {row, column});
+    }
+
+
+    public Piece getPiece(Square square){
+        return state.get(square);
+    }
+
+    public Piece getPiece(int[] coordinates){
+        return getPiece(getSquare(coordinates));
+    }
+
+    public Piece getPiece(int row, int column){
+        return getPiece(getSquare(row, column));
+    }
+
+
+    public int[] getCoordinates(Square square){
         return squares.get(square);
     }
 
-    public int[] getCoordinatesOf(Piece piece){
-        return getCoordinatesOf(state.inverse().get(piece));
+    public int[] getCoordinates(Piece piece){
+        return getCoordinates(state.inverse().get(piece));
     }
 
-    public void setPieceAt(Piece piece, Square square){
+    public int[] getCoordinates(int[] coordinates){
+        return getCoordinates(getSquare(coordinates));
+    }
+
+    public int[] getCoordinates(int row, int column){
+        return getCoordinates(getSquare(row, column));
+    }
+
+
+    public void setPiece(Square square, Piece piece){
         state.put(square, piece);
     }
 
-    public void setPieceAt(Piece piece, int[] coordinates){
-        setPieceAt(piece, getSquareAt(coordinates));
+    public void setPiece(int[] coordinates, Piece piece){
+        setPiece(getSquare(coordinates), piece);
+    }
+
+    public void setPiece(int row, int column, Piece piece){
+        setPiece(getSquare(row, column), piece);
+    }
+
+
+    public void removePiece(Square square){
+        state.remove(square);
     }
 
     public void removePiece(Piece piece){
-        removePieceFrom(getSquareOf(piece));
+        removePiece(getSquare(piece));
     }
 
-    public void removePieceFrom(Square square){
-        state.put(square, null);
+    public void removePiece(int[] coordinates){
+        removePiece(getSquare(coordinates));
     }
 
-    public void removePieceFrom(int[] coordinates){
-        removePieceFrom(getSquareAt(coordinates));
+    public void removePiece(int row, int column){
+        removePiece(getSquare(row, column));
     }
+
+
+    public Piece popPiece(Square square){
+        Piece popped = state.get(square);
+        removePiece(square);
+        return popped;
+    }
+
+    public Piece popPiece(Piece piece){
+        return popPiece(getSquare(piece));
+    }
+
+    public Piece popPiece(int[] coordinates){
+        return popPiece(getSquare(coordinates));
+    }
+
+    public Piece popPiece(int row, int column){
+        return popPiece(getSquare(row, column));
+    }
+
+
+    public Piece replacePiece(Square replace, Piece with){
+        Piece popped = popPiece(replace);
+        setPiece(replace, with);
+        return popped;
+    }
+
+    public Piece replacePiece(Piece replace, Piece with){
+        return replacePiece(getSquare(replace), with);
+    }
+
+    public Piece replacePiece(int[] coordinates, Piece with){
+        return replacePiece(getSquare(coordinates), with);
+    }
+
+    public Piece replacePiece(int row, int column, Piece with){
+        return replacePiece(getSquare(row, column), with);
+    }
+
 
     public ArrayList<Piece> getAllPieces() {
         ArrayList<Piece> res = new ArrayList<>();
