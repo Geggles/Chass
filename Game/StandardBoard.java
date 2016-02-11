e137cc0cb127bd2a857d52f76694de8e4fcdee62
Geggles <geggles@live.de>
2016-02-11 17:42:09 +0100

    Apply new setPiece signature

--- Game/StandardBoard.java
+++ Game/StandardBoard.java
@@ -14,17 +14,17 @@ public class StandardBoard extends PlayBoard {
         names.put(Color.BLACK, "B");
     }
     /**
-     * Color corresponds to the color of the army that starts out on the board
+     * Game.Color corresponds to the color of the army that starts out on the board
      */
     public StandardBoard(Color color) {
         super(names.get(color), color);
         setupPieces();
     }
 
-    public Square getSquareAt(int[] coordinates) {
+    public Square getSquare(int[] coordinates) {
         int height = coordinates[0];
         int width = coordinates[1] % 8;
-        return super.getSquareAt(new int[]{height, width});
+        return super.getSquare(new int[]{height, width});
     }
 
     private void setupPieces() {
@@ -38,7 +38,7 @@ public class StandardBoard extends PlayBoard {
         }
 
         for (int column = 0; column < 8; column++) {
-            setPieceAt(new Piece(Value.PAWN), new int[]{row, column});
+            setPiece(new int[]{row, column}, new Piece(Value.PAWN));
         }
 
         //other pieces
@@ -48,14 +48,14 @@ public class StandardBoard extends PlayBoard {
             row = 0;
         }
 
-        setPieceAt(new Piece(Value.ROOK), new int[]{row, 0});
-        setPieceAt(new Piece(Value.KNIGHT), new int[]{row, 1});
-        setPieceAt(new Piece(Value.BISHOP), new int[]{row, 2});
-        setPieceAt(new Piece(Value.QUEEN), new int[]{row, 3});
-        setPieceAt(new Piece(Value.KING), new int[]{row, 4});
-        setPieceAt(new Piece(Value.BISHOP), new int[]{row, 5});
-        setPieceAt(new Piece(Value.KNIGHT), new int[]{row, 6});
-        setPieceAt(new Piece(Value.ROOK), new int[]{row, 7});
+        setPiece(new int[]{row, 0}, new Piece(Value.ROOK));
+        setPiece(new int[]{row, 1}, new Piece(Value.KNIGHT));
+        setPiece(new int[]{row, 2}, new Piece(Value.BISHOP));
+        setPiece(new int[]{row, 3}, new Piece(Value.QUEEN));
+        setPiece(new int[]{row, 4}, new Piece(Value.KING));
+        setPiece(new int[]{row, 5}, new Piece(Value.BISHOP));
+        setPiece(new int[]{row, 6}, new Piece(Value.KNIGHT));
+        setPiece(new int[]{row, 7}, new Piece(Value.ROOK));
 
     }
 
@@ -64,23 +64,23 @@ public class StandardBoard extends PlayBoard {
      */
     public boolean isPinned(Square square) {
         StandardBoard board = (StandardBoard) square.board;
-        Piece piece = board.getPieceOn(square);
+        Piece piece = board.getPiece(square);
         if (piece == null || piece.color == board.color) return false;
         for (Piece p :
                 board.getAllPieces()) {
             if (p.value == Value.KING && p.color != piece.color) {
-                Square kingSquare = board.getSquareOf(p);
+                Square kingSquare = board.getSquare(p);
                 board.removePiece(piece);
                 boolean result = board.isUnderAttack(kingSquare, piece.color);
-                board.setPieceAt(piece, square);
+                board.setPiece(square, piece);
                 return result;
             }
         }
         throw new IllegalArgumentException("King has fallen over board!"); //<- should never be able to
-        // be thrown, but IDE bugs me.
+        // be thrown, but IDEA bugs me.
     }
     public boolean isPinned(Piece piece, StandardBoard board) {
-        return isPinned(board.getSquareOf(piece));
+        return isPinned(board.getSquare(piece));
     }
     /**
      * Check whether king is in check
@@ -90,7 +90,7 @@ public class StandardBoard extends PlayBoard {
         for (Piece piece :
                 getAllPieces()) {
             if (piece.value==Value.KING){
-                kingSquare = getSquareOf(piece);
+                kingSquare = getSquare(piece);
                 break;
             }
         }
