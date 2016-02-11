1de980c2eb559fd05bd039cbdf9b4ad73186add8
Geggles <geggles@live.de>
2016-02-11 18:16:06 +0100

    Board Gamma is implied

--- Game/HostageExchange.java
+++ Game/HostageExchange.java
@@ -2,10 +2,10 @@ package Game;
 
 public class HostageExchange extends Move{
     public final Value hostage;
-    public final Square square;
-    public HostageExchange(Color player, Value hostage, Square square){
+    public final int[] coordinates;
+    public HostageExchange(Color player, Value hostage, int[] coordinates){
         super(player, null);
         this.hostage = hostage;
-        this.square = square;
+        this.coordinates = coordinates;
     }
 }
