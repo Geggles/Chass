96340e8cbd9515febc06767f381786accce97fa3
Geggles <geggles@live.de>
2016-02-13 16:06:43 +0100

    Make attributes private and add accessors

--- Game/Piece.java
+++ Game/Piece.java
@@ -1,15 +1,40 @@
 package Game;
 
 public class Piece {
-    public Color color;
-    public boolean canNullMove = true;
-    public boolean canCastle = true;
+    private Color color;
+    private boolean canNullMove = true;
+    private boolean canCastle = true;
     public final Value value;
 
-    public Piece(Value value){
+    public Piece(Value value, Color color){
         this.value = value;
+        this.color = color;
         if (value == Value.KING){
-            this.canNullMove = false;
+            canNullMove = false;
         }
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void switchColor(){
+        color = color.opposite();
+    }
+
+    public boolean getCastleAbility(){
+        return canCastle;
+    }
+
+    public void cannotCastleAnymore(){
+        canCastle = false;
+    }
+
+    public boolean getNullMoveAbility(){
+        return canNullMove;
+    }
+
+    public void setNullMoveAbility(boolean value){
+        canNullMove = value;
     }
 }
