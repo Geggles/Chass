1aeef753e4386baba2c59dc298023c56a66afa05
Geggles <geggles@live.de>
2016-02-14 12:54:37 +0100

    Abilities are checked by history

--- Game/Piece.java
+++ Game/Piece.java
@@ -2,20 +2,11 @@ package Game;
 
 public class Piece {
     private Color color;
-    private boolean canNullMove = true;
-    private boolean canCastle = true;
-    // pawns can not promote anymore once they have been to prison
-    private boolean canPromote = true;
-    // pawns can move 2 squares only if they have not moved at all yet
-    private boolean canBoost = true;
     public final Value value;
 
     public Piece(Value value, Color color){
         this.value = value;
         this.color = color;
-        if (value == Value.KING){
-            canNullMove = false;
-        }
     }
 
     public Color getColor(){
@@ -24,37 +15,5 @@ public class Piece {
 
     public void switchColor(){
         color = color.opposite();
-    }
-
-    public boolean hasCastleAbility(){
-        return canCastle;
-    }
-
-    public void prohibitCastling(){
-        canCastle = false;
-    }
-
-    public boolean hasNullMoveAbility(){
-        return canNullMove;
-    }
-
-    public void setNullMoveAbility(boolean value){
-        canNullMove = value;
-    }
-
-    public boolean hasPromotionAbility(){
-        return canPromote;
-    }
-
-    public void prohibitPromotion(){
-        canPromote = false;
-    }
-
-    public boolean hasBoostAbility(){
-        return canPromote;
-    }
-
-    public void prohibitBoost(){
-        canPromote = false;
     }
 }
