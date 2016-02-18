f3e4fbbb1a637bd31d744bd08cd1ce954b892c98
Geggles <geggles@live.de>
2016-02-18 21:21:41 +0100

    None

--- GuiController.java
+++ GuiController.java
@@ -1,28 +1,4 @@
-import com.trolltech.qt.QSignalEmitter;
-import GUI.Signals;
-import com.trolltech.qt.gui.*;
-
-public class GuiController extends QSignalEmitter implements Controller{
-    /*Emit when file->save is invoked*/
-    private Signals signals = new Signals();
-    private Game.Controller gameController;
-
-    private QMainWindow mainWin;
-    public GuiController(){
-        String[] args = new String[0];
-        QApplication app = new QApplication(args);
-        setupGui();
-        signals.saveGame.connect(this, "saveGame()");
-        signals.exitApplication.connect(app, "exit()");
-        app.exec();
-    }
-
-    public int startGame(){
-        gameController = new Game.Controller();
-        gameController.newGame();
-        return 0;
-    }
-
+import com.trolltech.qt.QSignalEmitter; import GUI.Signals; import com.trolltech.qt.gui.*; public class GuiController extends QSignalEmitter implements Controller{ /*Emit when file->save is invoked*/ private Signals signals = new Signals(); private Game.Controller gameController; private QMainWindow mainWin; public GuiController(){ String[] args = new String[0]; QApplication app = new QApplication(args); setupGui(); signals.saveGame.connect(this, "saveGame()"); signals.exitApplication.connect(app, "exit()"); app.exec(); } public int startGame(){ gameController = new Game.Controller(); gameController.newGame(); return 0; }
     private void setupGui(){
         mainWin = new QMainWindow();
         QWidget centralWidget = new QWidget(mainWin);
